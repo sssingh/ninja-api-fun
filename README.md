66c19ed4ad89ef4de502aa94a28b0eaaf2588235
Sunil Surendra Singh <sunil.surendra,singh7@gmail.com>
2023-08-20 16:12:22 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,48 @@
+<a href="https://ninja-api-fun-sssingh.streamlit.app/"  target="_blank"><img src="https://img.shields.io/badge/open_app_in_streamlit-f63366?style=for-the-badge&logo=streamlit&logoColor=black" /></a>
+
+<img src="https://github.com/sssingh/nlp-text-paraphraser-gpt/blob/main/assets/title.png?raw=true" width="1000" height="300"/><br><br> 
+
+# Introduction  
+***A fun App demonstrating API use cases .***
+>This is a simple fun app that uses free `Ninja API` to create fun filled activities with less than 100 lines of `python` code. Four different API's are demonstrated by app, however ninja API provides large number of APIs that can be used for real-life practical cases such as `weather forecast data`, `stock prices`, `crypto prices`, `currency exchange rate`, `password generator`, `baby name generator` and so on. Look at [this page](https://api-ninjas.com/api) to get the complete list of APIs offered by Ninja API.  
+This app is using the free tier of the Ninja API service for demo purposes. Although app is using Ninja API, with minor tweaks it can be easily integrated with any other API service.
+
+# App Details
+The app contains two tabs: "ABOUT" and "ACTIVITY". 
+
+## ABOUT 
+This page, describes the app usage.
+    
+## ACTIVITY
+App currently includes four activities 
+
+#### 1. Dad Jokes
+With a click of button generate a funny (almost!) random `dad joke`... 
+
+<img src="https://github.com/sssingh/ninja-api-fun/blob/main/streamlit/assets/dadjokes.png?raw=true" width="1000" height="500"/><br>
+
+
+#### 2. Riddles
+Put your brain at work...
+
+<img src="https://github.com/sssingh/ninja-api-fun/blob/main/streamlit/assets/riddles.png?raw=true" width="1000" height="500"/><br>
+
+#### 3. Facts
+Really???
+
+<img src="https://github.com/sssingh/ninja-api-fun/blob/main/streamlit/assets/facts.png?raw=true" width="1000" height="500"/><br>
+
+#### 3. Trivia
+Not sure how to describe this, suggestions welcome...
+
+<img src="https://github.com/sssingh/ninja-api-fun/blob/main/assets/trivia.png?raw=true" width="1000" height="500"/><br>
+
+# Project Source
+[👉 Visit GitHub Repo](https://github.com/sssingh/ninja-api-fun)
+
+# Contact Me
+[![email](https://img.shields.io/badge/Gmail-D14836?style=for-the-badge&logo=gmail&logoColor=white)](mailto:sunil.surendra.singh7@gmail.com)
+[![twitter](https://img.shields.io/badge/twitter-1DA1F2?style=for-the-badge&logo=twitter&logoColor=white)](https://twitter.com/@thesssingh)
+[![linkedin](https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/sssingh/)
+[![website](https://img.shields.io/badge/web_site-8B5BE8?style=for-the-badge&logo=ko-fi&logoColor=white)](https://www.datamatrix-ml.com)
+
---
+++ app.py
@@ -0,0 +1,151 @@
+import streamlit as st
+import streamlit_nested_layout
+import requests
+import json
+
+
+###
+### Helper functions
+###
+def jsonify(response):
+    result_dict = json.loads(response.text)[0]
+    return result_dict
+
+
+def fetch_data(type):
+    ### prepare API key and URL
+    with st.spinner("Working..."):
+        api_key = st.secrets["RAPID_API_KEY"]
+        api_url = f"https://api.api-ninjas.com/v1/{type}"
+
+        ### invoke API and get the response
+        response = requests.get(url=api_url, headers={"X-Api-Key": api_key})
+        if response.status_code == requests.codes.ok:
+            data = jsonify(response)
+            status = "OK"
+        else:
+            data = {"code": response.status_code, "message": response.text}
+            status = "ERROR"
+    return status, data
+
+
+def display(text, font="sans-serif", color="white", size="25px"):
+    st.markdown(
+        f'<p style="font-family:{font}; color:{color}; font-size: {size};">{text}</p>',
+        unsafe_allow_html=True,
+    )
+
+
+def render_about_tab():
+    """Render the this page"""
+    with open("README.md", "r") as f:
+        text = f.read()
+    st.markdown(text, unsafe_allow_html=True)
+
+
+def show_dadkokes():
+    ### Dad Jokes
+    st.image("assets/dadjokes-logo.png", width=180)
+    with st.expander("Expand me to laugh out loud...", expanded=False):
+        if st.button("Get me a Dad Joke >>>"):
+            status, data = fetch_data("dadjokes")
+            if status == "OK":
+                joke = data["joke"]
+                display(text=joke + " 🤣 🤣 🤣", color="#FFD580")
+            elif status == "ERROR":
+                msg = f"({data['code']}): {data['message']}"
+                st.error(msg)
+
+
+def show_riddles():
+    ### Riddles section
+    st.divider()
+    st.image("assets/riddles-logo.png", width=180)
+    with st.expander("Find out how smart are u...", expanded=False):
+        if st.button("Test Me >>>"):
+            status, data = fetch_data("riddles")
+            if status == "OK":
+                title, question, answer = (
+                    data["title"],
+                    data["question"],
+                    data["answer"],
+                )
+                st.markdown("## Title:")
+                display(title, color="#ffffe0")
+                st.markdown("## Question:")
+                display(question, color="#ADD8E6")
+                with st.expander("Reveal Answer >>>", expanded=False):
+                    st.markdown("## Answer")
+                    display(answer, color="#90EE90")
+            elif status == "ERROR":
+                msg = f"({data['code']}): {data['message']}"
+                st.error(msg)
+
+
+def show_facts():
+    ### Facts section
+    st.divider()
+    st.image("assets/facts-logo.png", width=180)
+    with st.expander("Test your GK...", expanded=False):
+        if st.button("Give me a fact >>>"):
+            status, data = fetch_data("facts")
+            if status == "OK":
+                fact = data["fact"]
+                display(fact, color="#FFB6C1")
+            elif status == "ERROR":
+                msg = f"({data['code']}): {data['message']}"
+                st.error(msg)
+
+
+def show_trivia():
+    ### Trivia section
+    st.divider()
+    st.image("assets/trivia-logo.png", width=180)
+    with st.expander("Not a trivial task...", expanded=False):
+        if st.button("Show me a trivia >>>"):
+            status, data = fetch_data("trivia")
+            if status == "OK":
+                title, question, answer = (
+                    data["category"],
+                    data["question"],
+                    data["answer"],
+                )
+                st.markdown("## Category")
+                display(title, color="#CBC3E3")
+                st.markdown("## Question")
+                display(question, color="#DF00FF")
+                with st.expander("Reveal Answer >>>", expanded=False):
+                    st.markdown("## Answer")
+                    display(answer, color="#90EE90")
+            elif status == "ERROR":
+                msg = f"({data['code']}): {data['message']}"
+                print(msg)
+
+
+def render_activity_tab():
+    show_dadkokes()
+    show_riddles()
+    show_facts()
+    show_trivia()
+    st.divider()
+
+
+###
+### App entry point
+###
+if __name__ == "__main__":
+    ### page heading, logo and app banner
+    st.set_page_config(page_title="API Fun App", page_icon="assets/app-logo.gif")
+    col1, col2 = st.columns(2)
+    col1.title("A Fun app with The Ninja API")
+    col2.image("assets/app-logo.gif")
+
+    ### create tabs
+    tab_about, tab_activity = st.tabs(["ABOUT 👋", "ACTIVITY ✒️"])
+
+    ### render tabs
+    with tab_about:
+        render_about_tab()
+
+    with tab_activity:
+        render_activity_tab()
